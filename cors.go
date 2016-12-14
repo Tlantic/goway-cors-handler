ae3e0e12b2a40e1c04fc18d65f3d594953219d5d
Joel Braga <joelbraga.dev@gmail.com>
2016-12-14 11:34:19 +0000

    <feat>(Handler): first commit

---
+++ cors.go
@@ -0,0 +1,22 @@
+package goway_cors_handler
+
+import (
+	"net/http"
+	"github.com/andrepinto/goway/router"
+	. "github.com/andrepinto/goway/handlers"
+	"bytes"
+	"fmt"
+)
+
+
+//noinspection GoUnusedConst
+const NAME = "CORS"
+
+func Handle(route *router.Route, req *http.Request) (*HandlerError){
+	fmt.Println(req.Method)
+	if req.Method == "OPTIONS" {
+		req.Write(bytes.NewBufferString("OK"))
+		return NewHttpError(http.StatusOK, "OK")
+	}
+	return nil
+}
